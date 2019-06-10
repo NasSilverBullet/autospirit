a7bb4b0f37940a258b90e00e45f4b2498e6682cf
y.t <y.softvalley@gmail.com>
2019-06-10 15:02:35 +0900

    add NewWebDriver

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/NasSilverBullet/auto-insert_timestamps-on-teamsprit
 
 go 1.12
+
+require github.com/sclevine/agouti v3.0.0+incompatible
---
+++ pkg/webdriver/webdriver.go
@@ -0,0 +1,65 @@
+package webdriver
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"os"
+
+	"github.com/sclevine/agouti"
+)
+
+const (
+	appdir     = "/src/github.com/NasSilverBullet/auto-insert_timestamps-on-teamsprit"
+	configfile = "config.json"
+)
+
+type WebDriver struct {
+	Driver   *agouti.WebDriver
+	Browser  string `json:"browser"`
+	RootURL  string `json:"rooturl"`
+	ID       string `json:"id"`
+	Password string `json:"password"`
+}
+
+func NewWebDriver() (*WebDriver, error) {
+	b, err := openConfigFile()
+	if err != nil {
+		return nil, err
+	}
+	d := &WebDriver{}
+	err = json.Unmarshal(b, d)
+	if err != nil {
+		return nil, err
+	}
+	setDriver(d)
+	return d, err
+}
+
+func openConfigFile() ([]byte, error) {
+	gopath := os.Getenv("GOPATH")
+	if gopath == "" {
+		return nil, errors.New(fmt.Sprintf("\"%s\" is not defined, please define", gopath))
+	}
+	dir := gopath + appdir
+	if err := os.Chdir(dir); err != nil {
+		return nil, err
+	}
+	f, err := os.OpenFile(configfile, os.O_RDONLY, 0666)
+	if err != nil {
+		return nil, err
+	}
+	defer func() {
+		err = f.Close()
+	}()
+	byte, err := ioutil.ReadAll(f)
+	return byte, err
+}
+
+func setDriver(d *WebDriver) {
+	switch d.Browser {
+	case "chrome":
+		d.Driver = agouti.ChromeDriver(agouti.Browser("chrome"))
+	}
+}
